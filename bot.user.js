b55127ea8f8c5c137f20b9b15ce275d26b384902
emrealtinari <shanter513@gmail.com>
2015-08-25 16:01:24 +0300

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -23,7 +23,7 @@ SOFTWARE.*/
 // ==UserScript==
 // @name        AposBot
 // @namespace   AposBot
-// @include     http://agar.io/*
+// @include     http://agaryeri.com/*
 // @version     3.5682
 // @grant       none
 // @author      http://www.twitch.tv/apostolique
